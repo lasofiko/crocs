c40c46f0e2c2d47f661e95f1adfdc5b1b65b5a38
mrsskaa <mariabyrtseva@yandex.ru>
2026-05-11 15:25:59 +0300

    animation people

--- schedule-animation/src/types/header.ts
+++ schedule-animation/src/types/header.ts
@@ -3,4 +3,6 @@ export type HeaderProps = {
     time: string;
     guestsCount: number | null;
     onSaveSchedule: () => void;
+    /** Вне рабочих часов: затемнить весь экран, кроме времени и иконки часов */
+    screenDimmed?: boolean;
 };
